752450bbab694479800a2735232318c07f8ed33f
mustafahossameldin66-dotcom <mustafahossameldin66@gmail.com>
2026-08-18 14:45:39 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='rafiq-v90-shell-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./content-manifest.json','./content-local-manifest.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==location.origin)return;event.respondWith((async()=>{const cached=await caches.match(event.request);try{const res=await fetch(event.request);if(res.ok){caches.open(CACHE).then(c=>c.put(event.request,res.clone())).catch(()=>{})}return res}catch{return cached||new Response('Offline',{status:503,headers:{'Content-Type':'text/plain;charset=utf-8'}})}})())});

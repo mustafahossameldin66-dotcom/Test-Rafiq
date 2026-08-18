41d295a8a60dd6a56bfeb39bfc7f65b43062ab55
mustafahossameldin66-dotcom <mustafahossameldin66@gmail.com>
2026-08-18 11:08:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='rafiq-v66-shell-v1';
-const API_CACHE='rafiq-v66-api-v1';
+const CACHE='rafiq-v68-shell-v1';
+const API_CACHE='rafiq-v68-api-v1';
 self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./'])).catch(()=>{}).then(()=>self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg','./content-manifest.json','./content-local-manifest.json'])).catch(()=>{}).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![CACHE,API_CACHE].includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{

5a1ce8d9b9ba0b40af7dcdd4b764071e4b9f7866
mustafahossameldin66-dotcom <mustafahossameldin66@gmail.com>
2026-08-20 20:29:24 +0300

    Delete js directory

--- js/daily-home.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  const originalDaily=window.renderDailyHome;
-  window.renderDailyHome=function(){ if(typeof originalDaily==='function') originalDaily(); try{window.applyStableDailyContent?.()}catch{} };
-})();

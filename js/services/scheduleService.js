4320cd1e39067bc2d37511762a0a92166f5fb54f
mustafahossameldin66-dotcom <mustafahossameldin66@gmail.com>
2026-08-20 19:30:00 +0300

    Update scheduleService.js

--- js/services/scheduleService.js
+++ js/services/scheduleService.js
@@ -1,1 +1,8 @@
-
+const ScheduleService = {
+    getSchedule() { return StorageManager.get('schedule') || [['ورد القرآن', 'صباحًا'], ['مراجعة', 'مساءً']]; },
+    addSchedule(title, time) { const s = this.getSchedule(); s.push([title, time || 'مرن']); StorageManager.set('schedule', s); },
+    removeSchedule(index) { const s = this.getSchedule(); s.splice(index, 1); StorageManager.set('schedule', s); },
+    getReminders() { return StorageManager.get('reminders') || []; },
+    addReminder(title, time) { const r = this.getReminders(); r.push({ title, time }); StorageManager.set('reminders', r); },
+    removeReminder(index) { const r = this.getReminders(); r.splice(index, 1); StorageManager.set('reminders', r); }
+};

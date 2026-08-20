0d39f0c9e5acd791e67d41a4d3c76e261aff0f67
mustafahossameldin66-dotcom <mustafahossameldin66@gmail.com>
2026-08-20 19:27:48 +0300

    Update quranService.js

--- js/services/quranService.js
+++ js/services/quranService.js
@@ -1,1 +1,19 @@
-
+const QuranService = {
+    data: [], isLoaded: false, _loadPromise: null,
+    async load() {
+        if (this.isLoaded) return this.data;
+        if (this._loadPromise) return this._loadPromise;
+        this._loadPromise = fetch('quran-uthmani.json').then(res => res.json()).then(json => {
+            this.data = json; this.isLoaded = true; return this.data;
+        }).catch(error => { Utils.toast('حدث خطأ أثناء تحميل المصحف المحلي'); return []; });
+        return this._loadPromise;
+    },
+    getAllSurahs() { return this.data; },
+    getSurah(index) { return this.data[index - 1] || null; },
+    getVerse(surahIndex, ayahNumber) {
+        const surah = this.getSurah(surahIndex);
+        if (!surah || !surah.verses) return null;
+        return surah.verses.find(v => v.a === ayahNumber) || surah.verses[ayahNumber - 1];
+    },
+    getTotalSurahs() { return this.data.length; }
+};
